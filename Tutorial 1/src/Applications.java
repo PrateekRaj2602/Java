c82a968d773437bbb135a267161f8ac316bb1aa4
Prateek Singh <prateekraj2602@gmail.com>
2020-04-07 16:21:20 +0530

    first commit

---
+++ Tutorial 1/src/Applications.java
@@ -0,0 +1,23 @@
+
+public class Applications {
+
+	public static void main(String[] args) {
+		int myAge = 17;
+		short myShort = 1234;
+		byte myByte = 12;
+		long myLong = 11001L;
+		float myFloat = 13.314F;
+		double myDouble = 1344.44567;
+		char myChar = 'd';
+		boolean myBoolean = false;
+		
+		System.out.println(myAge);
+		System.out.println(myShort);
+		System.out.println(myByte);
+		System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+	}
+}
