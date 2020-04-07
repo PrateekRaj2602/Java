0f6765a3cc41779acf1e56c49c4f4648f936df3f
Prateek Singh <prateekraj2602@gmail.com>
2020-04-07 16:19:34 +0530

    first commit

---
+++ Non Primitive type(Reference Type)/src/Reference.java
@@ -0,0 +1,19 @@
+
+public class Reference {
+	public static void main(String[] args) {
+	int myInt = 17;
+	double myDouble = 17.9;
+    String text = "Hello";
+    String blank = " ";
+    String name = "Prateek";
+    String total = text + blank + name;
+    
+    
+    
+    System.out.println(text + blank + name);
+    System.out.println(total);
+    System.out.println(text + blank + name + "  " + myInt + ".");
+    System.out.println(total + " " + myDouble + ".");
+    
+	}
+}
